b2486160573492b5967bdd9b1c723444bed964ca
Gil <gilfpf@hotmail.com>
2018-09-29 16:43:31 +0100

    shelter_sql complete

---
+++ db/shelter.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS adoptions;
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS animals;
+
+CREATE TABLE  animals
+(
+id SERIAL8 primary key,
+name VARCHAR(255),
+admission_date INT8,
+age INT8,
+availability VARCHAR (255),
+type VARCHAR (255)
+);
+
+CREATE TABLE owners
+(
+id SERIAL8 primary key,
+name VARCHAR(255)
+);
+
+CREATE TABLE adoptions
+animal_id INT8 references animals(id),
+owner_id INT8 references owners(id)
+);
